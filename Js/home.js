b20dd131dc0f13da79a90aa2a05a1a8aa28a7554
arnav <arnav1776@gmail.com>
2021-08-10 17:07:14 +0530

    numbers is js

---
+++ Js/home.js
@@ -0,0 +1,32 @@
+console.log('hello');
+
+//alert('Hello,this is Arnav');
+
+//Inline Comment
+
+// Variables
+var b = 'smoothie';
+console.log(b);
+
+var num = 45;
+console.log(num);
+
+//var age = prompt('What is your age?');
+//document.getElementById('someText').innerHTML = age;
+
+// Numbers is Js
+var num1 = 10;
+//console.log(5 * 10);
+num1 = num1 + 1;
+num1++;
+//decrement
+num1--;
+console.log(num1);
+
+//divide,multiply *, remainder %
+console.log(num1 % 5);
+console.log(num1 / 5);
+
+//increment decrement by any number
+num1 += 10;
+console.log(num1);
