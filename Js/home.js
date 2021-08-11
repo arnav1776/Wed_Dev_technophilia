db78e2ec6f6d38ed500d4cea92e20db589f80681
arnav <arnav1776@gmail.com>
2021-08-11 10:43:33 +0530

    functions

--- Js/home.js
+++ Js/home.js
@@ -30,3 +30,31 @@ console.log(num1 / 5);
 //increment decrement by any number
 num1 += 10;
 console.log(num1);
+
+/* Functions
+1. create a function
+2. call a function
+*/
+function fun() {
+    console.log('this is a function');
+}
+fun();
+
+function greeting(yourName) {
+    var result = 'Hello ' + yourName; // String Concatenation
+    console.log(result);
+}
+
+var Name = prompt('What is your name?');
+greeting(Name);
+
+// arguments
+
+function sumNum(a, b) {
+    var result = a + b;
+    console.log(result);
+}
+sumNum(3, 5);
+//sumNum('Hello ' + 'arnav ');
+
+
