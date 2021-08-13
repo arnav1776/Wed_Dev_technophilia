71ca94400c8e9ab14f926d68685891c233844760
arnav <arnav1776@gmail.com>
2021-08-13 11:39:39 +0530

    array

--- Js/home.js
+++ Js/home.js
@@ -95,9 +95,47 @@ console.log(fruit[2]);
 console.log(fruit.split(''));
 console.log(fruit.split(','));
 
+// Array
+let fruits = ['banana', 'apple', 'orange', 'pineapples'];
+let fruits1 = new Array('banana', 'apple', 'orange', 'pineapples');
 
+console.log(fruits[1]);
 
+fruits[0] = 'pear';
+console.log(fruits);
 
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// array methods
+console.log('to string', fruits.toString());
+console.log(fruits.join(' - '));
+console.log(fruits.pop(), fruits);
+console.log(fruits.push('blackberries'), fruits);
+fruits[fruits.length];
+console.log(fruits[4]);
+fruits.shift();
+console.log(fruits);
+fruits.unshift('kiwi');
+console.log(fruits);
+
+let vegetables = ['asparagus', 'tomato', 'brocoli'];
+let allGroceries = fruits.concat(vegetables);
+console.log(allGroceries);
+console.log(allGroceries.slice(1, 4));
+console.log(allGroceries.reverse());
+console.log(allGroceries.sort());
+
+let someNumbers = [5, 10, 2, 25, 3, 255, 1, 2, 5, 334, 321, 2];
+console.log(someNumbers.sort(function(a, b) {return a-b}));
+console.log(someNumbers.sort(function(a, b) {return b-a}));
+
+let emptyArray = new Array(); 
+for (let num = 0; num < 10; num++) {
+    emptyArray.push(num);
+}
+console.log(emptyArray);
 
 
 
