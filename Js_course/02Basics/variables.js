40b36a3914565df506522990c98c6473c1611135
Arnav Sharma <arnav1776@gmail.com>
2021-09-14 10:08:54 +0530

    varibles

---
+++ Js_course/02Basics/variables.js
@@ -0,0 +1,15 @@
+var fullName = "Arnav Sharma";
+
+var courseName = "JavaScript Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "credit card";
