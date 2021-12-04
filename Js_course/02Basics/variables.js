bc1d36cf6946f4fdb751c5afc0d80b3edde444b4
Arnav Sharma <arnav1776@gmail.com>
2021-12-04 10:41:29 +0530

    interpolation

--- Js_course/02Basics/variables.js
+++ Js_course/02Basics/variables.js
@@ -5,11 +5,17 @@ var courseName = "JavaScript Bootcamp";
 var isLoggedIn = false;
 
 var loggedCount = 34;
+var uid = 101;
+var email = "abc@xyz.com";
 
 console.log(loggedCount);
 
-console.log("loggedCount");
 
-var paymentMode;
+//fullName = prompt("Enter your Name");
+console.log(fullName);
+console.log(`
+  With unique id: ${uid}
+  User is: ${fullName}
+  and his email id: ${email}
 
-paymentMode = "credit card";
+`);
