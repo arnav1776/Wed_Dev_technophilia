d995798c29b8a13c566d50618d74d551d08f1782
Arnav Sharma <arnav1776@gmail.com>
2021-12-17 20:03:26 +0530

    conditionals

---
+++ Js_course/02Basics/Conditionals.js
@@ -0,0 +1,19 @@
+var temperature;
+
+// ToDo go to google and get the data
+
+temperature = 49;
+// var result = temperature < 20;
+// console.log(result);
+
+if (temperature < 20) {
+    console.log("It's cold");
+}
+
+if (temperature < 30) {
+    console.log("It's moderate outside");
+}
+
+else {
+    console.log("It's Hot");
+}
