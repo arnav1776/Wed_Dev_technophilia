33a66686eec2121b7b637930c4498a84833ac20a
Arnav Sharma <arnav1776@gmail.com>
2022-01-08 10:52:52 +0530

    es6

--- Scaler/main.js
+++ Scaler/main.js
@@ -1,20 +1,30 @@
 // DOM -> Document Object Model
 
-const document = {
-    getElementById: function () {
-        // ... have some function
-    },
-    innerHTML,
-    style,
-};
-
-
-function clickAlert(arg = "Log In Successful") {
-    alert(arg);
-
-    console.log(arg);
+function clickAlert() {
+    alert("Log In Successful");
     }
 
 const buttonTag = document.getElementById("button-id");
+// const divTag = document.getElementById("div-id");
+const input1 = document.getElementById("input-1");
+const input2 = document.getElementById("input-2");
 
-buttonTag.onclick = clickAlert;
+buttonTag.onclick = function() {
+    // divTag.innerHTML = "Value Changed";
+    var initialText = input1.value;
+    input2.value = initialText;
+    input1.value = "";
+};
+
+// ES -> ECMAScript
+
+let x = 1; // var x;
+let x;
+const z = 1;
+
+// Arrow Function => {}
+// function abc() {
+
+// }
+
+const abc = () => {}
